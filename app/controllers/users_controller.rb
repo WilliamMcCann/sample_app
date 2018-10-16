92149b046a58dfc9c6e3b6129dd6f217fca707b7
William McCann <nwilliammccann@gmail.com>
2018-10-16 15:30:26 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -19,11 +26,54 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    #WM /- Based on listing 10.28 it looks like this line needs to be commented out
+    #@user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+  else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
+    # Before filters
+
+    # Confirms a logged-in user.
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        return redirect_to login_url
+      end
+    end
+
+    #confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    #confirms an admin user
+    def admin_user
+      return redirect_to login_url unless logged_in?
+      redirect_to(root_url) if (!logged_in? || !current_user.try(:admin?))
+    end
 end
