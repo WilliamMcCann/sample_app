20a2d6e098d5bb45705e93498fa78349aedbe89d
William McCann <nwilliammccann@gmail.com>
2018-10-25 01:29:10 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  #trouble shooting the host name
+  host = 'Rails_Tutorial_User-wmccann.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }  
+  #host = 'localhost:3000'
+  #config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
